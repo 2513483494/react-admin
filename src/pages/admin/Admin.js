0cf28aa1a68f0e69ff957034e05fe134db872528
xingjiaqi02 <xingjiaqi02@meituan.com>
2021-05-19 14:20:46 +0800

    feat: 新增图表模块

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import memoryUtils from '../../utils/memoryUtils'
-import {Redirect, Route, Switch} from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import { Layout } from 'antd';
 import LeftNav from '../../components/left-nav/left-nav'
 import Header from '../../components/header/header'
@@ -12,6 +12,7 @@ import Role from '../role/role'
 import Bar from '../charts/bar'
 import Line from '../charts/line'
 import Pie from '../charts/pie'
+import NotFound from '../notFound/NotFound';
 
 const { Footer, Sider, Content } = Layout;
 
@@ -22,26 +23,27 @@ export default class Admin extends Component {
             return <Redirect to='/login'></Redirect>
         }
         return (
-            <Layout style={{'height':'100%'}}>
+            <Layout style={{ 'height': '100%' }}>
                 <Sider>
                     <LeftNav></LeftNav>
                 </Sider>
                 <Layout>
                     <Header>Header</Header>
-                    <Content style={{margin:20, backgroundColor: '#fff'}}>
+                    <Content style={{ margin: 20, backgroundColor: '#fff' }}>
                         <Switch>
-                            <Route path='/home' component={Home}/>
-                            <Route path='/category' component={Category}/>
-                            <Route path='/product' component={Product}/>
-                            <Route path='/user' component={User}/>
-                            <Route path='/role' component={Role}/>
-                            <Route path='/charts/bar' component={Bar}/>
-                            <Route path='/charts/line' component={Line}/>
-                            <Route path='/charts/pie' component={Pie}/>
-                            <Redirect to='/home'/>
+                            <Redirect exact from='/' to='/home' />
+                            <Route path='/home' component={Home} />
+                            <Route path='/category' component={Category} />
+                            <Route path='/product' component={Product} />
+                            <Route path='/user' component={User} />
+                            <Route path='/role' component={Role} />
+                            <Route path='/charts/bar' component={Bar} />
+                            <Route path='/charts/line' component={Line} />
+                            <Route path='/charts/pie' component={Pie} />
+                            <Route component={NotFound}/>
                         </Switch>
                     </Content>
-                    <Footer style={{textAlign :'center', color: 'grey'}}>这里是我的世界。</Footer>
+                    <Footer style={{ textAlign: 'center', color: 'grey' }}>这里是我的世界。</Footer>
                 </Layout>
             </Layout>
         )
